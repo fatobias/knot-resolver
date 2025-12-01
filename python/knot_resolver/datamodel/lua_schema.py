336e022a77c9a81755fc5093c79d5e35a3060f05
Vladimír Čunát <vladimir.cunat@nic.cz>
2025-12-01 20:36:20 +0100

    datamodel: add /lua/policy-script*

--- python/knot_resolver/datamodel/lua_schema.py
+++ python/knot_resolver/datamodel/lua_schema.py
@@ -10,14 +10,20 @@ class LuaSchema(ConfigSchema):
 
     ---
     script_only: Ignore declarative configuration and use only Lua script or file defined in this section.
-    script: Custom Lua configuration script.
-    script_file: Path to file that contains Lua configuration script.
+    script: Custom Lua configuration script (for workers).
+    script_file: Path to file that contains Lua configuration script (for workers).
+    policy_script: Custom Lua configuration script for policy-loader.
+    policy_script_file: Path to file that contains Lua configuration script for policy-loader.
     """
 
     script_only: bool = False
     script: Optional[str] = None
     script_file: Optional[ReadableFile] = None
+    policy_script: Optional[str] = None
+    policy_script_file: Optional[ReadableFile] = None
 
     def _validate(self) -> None:
         if self.script and self.script_file:
             raise ValueError("'lua.script' and 'lua.script-file' are both defined, only one can be used")
+        if self.policy_script and self.policy_script_file:
+            raise ValueError("'lua.policy-script' and 'lua.policy-script-file' are both defined, only one can be used")
